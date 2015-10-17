8d9cdb799ca3fbf3990a75c82597e4a1567d901d
Ciro Meneses <ciroccuva@hotmail.com>
2015-10-17 03:23:10 -0300

    v 1.0.1

--- VELHAC.h
+++ VELHAC.h
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <allegro.h>
 
+#define  XIZ   2
+#define  BOLA  1
+#define  NADA  0
+
+void CARREGA_OBJETOS();
+void INICIA_ALLEGRO();
+
 BITMAP *buffer, *img_cursor, *img_X, *img_O, *img_tabuleiro; // double buffer
 
 //----O TAB[10] É FORMADO ASSIM
@@ -9,30 +16,34 @@ BITMAP *buffer, *img_cursor, *img_X, *img_O, *img_tabuleiro; // double buffer
 //TAB[7]||TAB[8]||TAB[9]
 //
 int TAB[10], CONTA_HUMANO, CONTA_PC;
+int contador, fator_empate = 1, conta, VENCEU, PERDEU, EMPATOU, resultado[3];
+
 
 //FUNÇÃO QUE INICIA TOTALMENTE A ALLEGRO
-INICIA_ALLEGRO()
-{
-      allegro_init();
-      install_keyboard();
-      install_timer();
-      install_mouse();
-//Ajusta video para 16 bits de cores
- set_color_depth(32);
+void INICIA_ALLEGRO() {
+  int x;
+  for (x = 0; x < 3; x++)
+      resultado[x] = 0;// 0 = vitoria, 1 = derrota, 2 = empate
 
-//Autodetecta a placa  de video e usa resolucao de 640x480
- set_gfx_mode(GFX_AUTODETECT_WINDOWED,640,480,0,0);
+  allegro_init();
+  install_keyboard();
+  install_timer();
+  install_mouse();
+  //Ajusta video para 16 bits de cores
+  set_color_depth(32);
 
-if (install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, 0) != 0) {
- exit(-1);
+  //Autodetecta a placa  de video e usa resolucao de 640x480
+  set_gfx_mode(GFX_AUTODETECT_WINDOWED,640,480,0,0);
+
+  if (install_sound(DIGI_AUTODETECT, MIDI_AUTODETECT, 0) != 0) {
+    exit(-1);
+  }
 }
-}
 
-CARREGA_OBJETOS()
-{
-buffer = create_bitmap(640,480);
-img_cursor = load_bmp("cursor/mira2.bmp", NULL);
-img_X = load_bmp("img/X.bmp", NULL);
-img_O = load_bmp("img/bola.bmp", NULL);
-img_tabuleiro = load_bmp("img/tabuleiro.bmp", NULL);
+void CARREGA_OBJETOS() {
+  buffer = create_bitmap(640,480);
+  img_cursor = load_bmp("cursor/mira2.bmp", NULL);
+  img_X = load_bmp("img/X.bmp", NULL);
+  img_O = load_bmp("img/bola.bmp", NULL);
+  img_tabuleiro = load_bmp("img/tabuleiro.bmp", NULL);
 }
